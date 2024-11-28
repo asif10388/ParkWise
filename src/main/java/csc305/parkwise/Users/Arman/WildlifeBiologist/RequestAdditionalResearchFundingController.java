5c222d95d15c7eb332dfff618a161e1fe2e09060
Arman506li <152505802+Arman506li@users.noreply.github.com>
2024-11-29 03:29:53 +0600

    upgrade

--- src/main/java/csc305/parkwise/Users/Arman/WildlifeBiologist/RequestAdditionalResearchFundingController.java
+++ src/main/java/csc305/parkwise/Users/Arman/WildlifeBiologist/RequestAdditionalResearchFundingController.java
@@ -1,4 +1,58 @@
 package csc305.parkwise.Users.Arman.WildlifeBiologist;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+
 public class RequestAdditionalResearchFundingController {
+    @javafx.fxml.FXML
+    public void supplementaryResearchGoalTextfield(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void PublicInterestAndAwarenessOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void DependenceOnDonorFundingOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void InsufficientGrantOpportunitiesOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void DateDatePickerOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackButtonOnMouseClick(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void SustainabilityofFundingOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void TechnologyAndEquipmentCostsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BudgetConstraintsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void PersonnelAndTrainingCostsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void AmountTextfieldOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SubmitButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void RegulatoryComplianceCostsOnAction(ActionEvent actionEvent) {
+    }
 }
