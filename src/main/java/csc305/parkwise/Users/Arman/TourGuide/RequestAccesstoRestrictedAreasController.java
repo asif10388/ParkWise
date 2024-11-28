d3a7ec86794ec4b02ef69db7c488ef253ff750c5
Arman506li <152505802+Arman506li@users.noreply.github.com>
2024-11-29 00:55:19 +0600

    upgrade

---
+++ src/main/java/csc305/parkwise/Users/Arman/TourGuide/RequestAccesstoRestrictedAreasController.java
@@ -0,0 +1,50 @@
+package csc305.parkwise.Users.Arman.TourGuide;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+
+public class RequestAccesstoRestrictedAreasController {
+    @javafx.fxml.FXML
+    public void InseptionRadioButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void UserIDTextfieldOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SecurityCheckRadioButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ExplainTextfieldOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackButtonOnMouseClick(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void MaintenanceRadioButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void AnimalCareRadioButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ReseacrhRadioButtonOnaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SubmitButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void LocationComboBoxOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void DateDatepickerOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/csc305/parkwise/Users/Arman/TourGuide/UpdateEndangeredSpeciesListController.java
@@ -0,0 +1,28 @@
+package csc305.parkwise.Users.Arman.TourGuide;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.scene.control.Label;
+
+public class UpdateEndangeredSpeciesListController {
+
+    @javafx.fxml.FXML
+    public void StatusTextfieldOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SpeciesNameComboBoxOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SubmitButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void LocationComboBoxOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackButtonOnMouseClick(Event event) {
+    }
+}
