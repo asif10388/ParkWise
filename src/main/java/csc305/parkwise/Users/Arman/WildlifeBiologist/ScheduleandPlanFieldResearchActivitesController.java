efbeae748fd3bdbd43119567776943cdfa787bdd
Arman506li <152505802+Arman506li@users.noreply.github.com>
2024-11-30 00:41:31 +0600

    upgrade

--- src/main/java/csc305/parkwise/Users/Arman/WildlifeBiologist/ScheduleandPlanFieldResearchActivitesController.java
+++ src/main/java/csc305/parkwise/Users/Arman/WildlifeBiologist/ScheduleandPlanFieldResearchActivitesController.java
@@ -1,4 +1,34 @@
 package csc305.parkwise.Users.Arman.WildlifeBiologist;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+
 public class ScheduleandPlanFieldResearchActivitesController {
+    @javafx.fxml.FXML
+    public void StartdateDatepickerOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void AssingnedPersonalComboBoxOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ResearchTypeTextfieldOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackButtonOnMouseClick(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void EndDateDatePpickerOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SubmitButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void LocationComboBoxOnAction(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/csc305/parkwise/Users/Arman/WildlifeBiologist/SubmitResearchProposalforApprovalController.java
+++ src/main/java/csc305/parkwise/Users/Arman/WildlifeBiologist/SubmitResearchProposalforApprovalController.java
@@ -1,4 +1,38 @@
 package csc305.parkwise.Users.Arman.WildlifeBiologist;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+
 public class SubmitResearchProposalforApprovalController {
+    @javafx.fxml.FXML
+    public void ProfessionComboboxOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SubmissionDateDatepickerOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackButtonOnMouseClick(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void TitleTextfieldOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void NameTextfieldOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ExpectedOutcomeOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ObjectivesTextfieldOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SubmitButtonOnAction(ActionEvent actionEvent) {
+    }
 }
