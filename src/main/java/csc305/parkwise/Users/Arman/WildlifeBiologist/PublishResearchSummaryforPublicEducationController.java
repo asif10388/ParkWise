e3a6afd297bce03f98c05c6f2b2249b0a59e7188
Arman506li <152505802+Arman506li@users.noreply.github.com>
2024-11-30 01:48:42 +0600

    upgrade

--- src/main/java/csc305/parkwise/Users/Arman/WildlifeBiologist/PublishResearchSummaryforPublicEducationController.java
+++ src/main/java/csc305/parkwise/Users/Arman/WildlifeBiologist/PublishResearchSummaryforPublicEducationController.java
@@ -1,4 +1,38 @@
 package csc305.parkwise.Users.Arman.WildlifeBiologist;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+
 public class PublishResearchSummaryforPublicEducationController {
+    @javafx.fxml.FXML
+    public void ResearchTitleTextFieldOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void UserIDComboBoxOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void PublicationDateDatePickerOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackButtonOnMouseClick(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void ResearchHighlightTextFieldOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void NameTextFieldOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SubmitButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void TargetAudienceComboBoxOnAction(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/csc305/parkwise/Users/Arman/WildlifeBiologist/UpdateTaggedAnimalTrackingInformationController.java
+++ src/main/java/csc305/parkwise/Users/Arman/WildlifeBiologist/UpdateTaggedAnimalTrackingInformationController.java
@@ -1,4 +1,38 @@
 package csc305.parkwise.Users.Arman.WildlifeBiologist;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+
 public class UpdateTaggedAnimalTrackingInformationController {
+    @javafx.fxml.FXML
+    public void MovementPatternTextFieldOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackButtonOnMouseClick(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void TimeTextfieldOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SubmitButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void TaggedAnimalIDComboBoxOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BehaviorTextFieldOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void LocationComboBoxOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void DateDatepickerOnAction(ActionEvent actionEvent) {
+    }
 }
