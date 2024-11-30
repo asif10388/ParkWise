7d4807fde139bbae818abbfc7d849aedb7cfc9ab
Arman506li <152505802+Arman506li@users.noreply.github.com>
2024-11-30 16:12:32 +0600

    upgrade

--- src/main/java/csc305/parkwise/Users/Arman/TourGuide/RegisterANewTourGroupController.java
+++ src/main/java/csc305/parkwise/Users/Arman/TourGuide/RegisterANewTourGroupController.java
@@ -1,4 +1,62 @@
 package csc305.parkwise.Users.Arman.TourGuide;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+
 public class RegisterANewTourGroupController {
+    @javafx.fxml.FXML
+    public void AdditionalNotesTextFieldOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void PetFriendlyRequestCheckBoxOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void PhotographyPreferencesCheckBoxOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void GroupSizeComboBoxOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void AccessibilityNeedsCheckBoxOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void DateDatePickerOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void NumberOfPeopleTextFieldOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void TourTypeComboBoxOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SubmitButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void GroupNameTextFieldOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void AdditionalNotesCheckBoxOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackButtonOnMouseClick(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void TimeSlotComboBoxOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void DietaryPreferencesOrRestrictionsCheckBoxOnAction(ActionEvent actionEvent) {
+    }
 }
