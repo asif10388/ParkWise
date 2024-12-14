f6e1c4756a0801626c15de28220bc555c6bad79d
romtahenajoariaruhi <romtahenaruhi@gmail.com>
2024-12-14 22:31:25 +0600

    fxml File

---
+++ src/main/java/csc305/parkwise/Users/Ruhi/ParkVisitor/BookingDetails.java
@@ -0,0 +1,20 @@
+package csc305.parkwise.Users.Ruhi.ParkVisitor;
+
+public class BookingDetails
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void onTimebutton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onDatebutton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onClickbutton(ActionEvent actionEvent) {
+    }
+}
